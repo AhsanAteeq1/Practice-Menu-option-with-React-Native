f2d65653f07caf77853a0b7529da5dcc5d0e7044
Ahsan Muhammad <ahsanateeq11@gmail.com>
2022-10-23 21:21:42 -0500

    Update App.js

--- Components/App.js
+++ Components/App.js
@@ -1,1 +1,18 @@
+// App.js
+import { StyleSheet, Text, View } from "react-native";
+import SimpleMenu from "./components/SimpleMenu";
 
+export default function App() {
+ return (
+   <View style={styles.container}>
+     <SimpleMenu />
+   </View>
+ );
+}
+
+const styles = StyleSheet.create({
+ container: {
+   flex: 1,
+   backgroundColor: "#fff",
+ },
+});
